a84c2b493be23b809cbf7c27d32011a662253b17
a104448 <a104448@alunos.uminho.pt>
2025-03-06 17:43:21 +0000

    TPC4

--- TPC4/tpc4.py
+++ TPC4/tpc4.py
@@ -1,79 +1,104 @@
 import sys
+import re 
 import ply.lex as lex
 
 # Palavras reservadas
-keywords = {
-    'select': 'SELECT',
-    'where': 'WHERE',
-    'limit': 'LIMIT'
-}
+tokens = (
+    'SELECT', 
+    'WHERE', 
+    'LIMIT', 
+    'TYPE',
+    'VAR', 
+    'CLASSE', 
+    'NUMBER', 
+    'STRING', 
+    'POINT', 
+    'LBRACE', 
+    'RBRACE',
+    'ID'
+    )
 
-# Palavras reservadas sensíveis a maiúsculas
-case_sensitive_keywords = {
-    'a': 'TYPE'
-}
-
-# Lista de tokens
-tokens = ['VAR', 'CLASS', 'NUMBER', 'STRING', 'DOT', 'LBRACE', 'RBRACE', 'IDENTIFIER'] + \
-         list(keywords.values()) + list(case_sensitive_keywords.values())
-
-# Tokens fixos
-t_DOT = r'\.'
+t_POINT = r'\.'
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
 
-def t_VAR(t):
-    r'\?[a-zA-Z_]\w*'
+# Procura SELECT
+def t_SELECT(t):
+    r'(?i)select'
     return t
 
-def t_CLASS(t):
+# Procura WHERE
+def t_WHERE(t):
+    r'(?i)where'
+    return t
+
+# Procura LIMIT
+def t_LIMIT(t):
+    r'(?i)limit'
+    return t
+
+# Procura TYPE
+def t_TYPE(t):
+    r'\ba\b'
+    return t
+
+# Procura CLASSE 
+def t_CLASSE(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*:[a-zA-Z_][a-zA-Z_0-9]*'
     return t
 
-def t_STRING(t):
-    r'\".*?\"(@[a-zA-Z]+)?'
-    return t
-
+# Procura NUMBER
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_IDENTIFIER(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    lower_value = t.value.lower()
-    if lower_value in keywords:
-        t.type = keywords[lower_value]
-    elif t.value in case_sensitive_keywords:
-        t.type = case_sensitive_keywords[t.value]
+# Procura STRING
+def t_STRING(t):
+    r'\".*?\"(@[a-zA-Z]+)?'
     return t
 
+# Procura COMMENT e ignora
 def t_COMMENT(t):
     r'\#.*'
-    pass  # Comentários são ignorados
+    pass # descartar os comentários
 
+# Procura VAR
+def t_VAR(t):
+    r'\?[a-zA-Z_]\w*'
+    return t
+
+# Procura ID
+def t_ID(t):
+    r'[a-zA-Z_]\w*'
+    return t
+
+# Procura New Line
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+# Ignora espaços e tabs    
 t_ignore = ' \t'
 
+# Define o resto como erro
 def t_error(t):
-    print(f"Caractere inválido: '{t.value[0]}'")
+    print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 # Criar o lexer
 lexer = lex.lex()
 
-def run_lexer(input_data):
-    lexer.input(input_data)
-    for token in lexer:
-        print(token)
+
+def test_lexer(data):
+    lexer.input(data)
+    for tok in lexer:
+        print(tok)
 
 def main():
-    with open("input.txt", "r") as file:
-        user_input = file.read()
-    run_lexer(user_input)
+    with open("input.txt", "r", encoding="utf-8") as file:
+        query = file.read()  # Lê o conteúdo do ficheiro
+    test_lexer(query)  # Processa os tokens normalmente
 
 if __name__ == "__main__":
     main()
